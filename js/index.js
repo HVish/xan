19b617239413888cc8b20deff2821f8dfa5bdec0
HVish <vishnusingh1729@gmail.com>
2015-09-27 13:58:18 +0530

    add some nice social icons

--- js/index.js
+++ js/index.js
@@ -1,11 +1,55 @@
 var  mn = $(".main-nav");
     mns = "main-nav-scrolled";
     hdr = $('header').height();
-
+function OnLoad(){
+	if(window.innerWidth >= 480){
+		$('body').css("padding-top",hdr);
+	}
+	else{
+		$('body').css("padding-top",hdr + $('.fb').height());
+	}
+}
 $(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
+  if(window.innerWidth < 480){
+		$('body').css("padding-top",hdr + $('.fb').height());
+	 	
+	 	if( $(this).scrollTop() > (hdr + $('.fb').height())) {
+	    	mn.addClass(mns);
+	 	} 
+	 	else {
+			mn.removeClass(mns);
+	  	}
+	}
+	else{
+		$('body').css("padding-top",hdr);
+		
+		if( $(this).scrollTop() > hdr ) {
+		    mn.addClass(mns);
+		  } 
+		  else {
+		    mn.removeClass(mns);
+		  }
+	}
+});
+$(window).resize(function() {
+	if(window.innerWidth < 480){
+		$('body').css("padding-top",hdr + $('.fb').height());
+	 	
+	 	if( $(this).scrollTop() > (hdr + $('.fb').height())) {
+	    	mn.addClass(mns);
+	 	} 
+	 	else {
+			mn.removeClass(mns);
+	  	}
+	}
+	else{
+		$('body').css("padding-top",hdr);
+		
+		if( $(this).scrollTop() > hdr ) {
+		    mn.addClass(mns);
+		  } 
+		  else {
+		    mn.removeClass(mns);
+		  }
+	}
 });
